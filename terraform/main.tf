8a24b576a50892353ac70459e6f71cad20b69eea
Daniel Poss <rixxroid@gmail.com>
2025-01-16 23:19:59 -0600

    Remove unused internet gateway and route table resources from Terraform configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,24 +8,6 @@ resource "aws_subnet" "public" {
   map_public_ip_on_launch = true
 }
 
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = var.public_route_cidr
-    gateway_id = aws_internet_gateway.gw.id
-  }
-}
-
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
-}
-
 resource "aws_ecs_cluster" "main" {
   name = var.ecs_cluster_name
 }
