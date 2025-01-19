337615892e42c45e08b1189fa7125c74452ac6b3
Daniel Poss <rixxroid@gmail.com>
2025-01-19 16:58:38 -0600

    Creating IAM roles

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -13,3 +13,76 @@ resource "aws_ecs_service" "app" {
     assign_public_ip = true
   }
 }
+
+resource "aws_iam_role" "ecs_task_role" {
+  name = "mr-bot-ecs-task-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com",
+        },
+        Action = "sts:AssumeRole",
+      },
+    ],
+  })
+}
+
+resource "aws_iam_role_policy" "ecs_task_policy" {
+  name = "mr-bot-ecs-task-policy"
+  role = aws_iam_role.ecs_task_role.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "appconfig:StartConfigurationSession",
+          "appconfig:GetLatestConfiguration"
+        ],
+        Resource = "*",
+      },
+    ],
+  })
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "mr-bot-ecs-task-execution-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com",
+        },
+        Action = "sts:AssumeRole",
+      },
+    ],
+  })
+}
+
+resource "aws_iam_role_policy" "ecs_task_execution_policy" {
+  name = "mr-bot-ecs-task-execution-policy"
+  role = aws_iam_role.ecs_task_execution_role.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "secretsmanager:*"
+        ],
+        Resource = "*",
+      },
+    ],
+  })
+}
