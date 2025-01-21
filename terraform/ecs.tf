8b206391b592c6ef11cbe34b0d30f0503fd15c86
Daniel Poss <rixxroid@gmail.com>
2025-01-21 00:26:13 -0600

    Add ECS task definition and network configuration to service

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -3,14 +3,45 @@ resource "aws_ecs_cluster" "main" {
 }
 
 resource "aws_ecs_service" "app" {
-  name           = var.mr_bot_service_name
-  cluster        = aws_ecs_cluster.main.id
-  desired_count  = 1
-  launch_type    = "FARGATE"
-  propagate_tags = "TASK_DEFINITION"
+  name            = var.mr_bot_service_name
+  cluster         = aws_ecs_cluster.main.id
+  desired_count   = 1
+  launch_type     = "FARGATE"
+  propagate_tags  = "TASK_DEFINITION"
+  task_definition = aws_ecs_task_definition.placeholder_task.arn
   deployment_controller {
     type = "EXTERNAL"
   }
+
+  network_configuration {
+    subnets          = [aws_subnet.public.id]
+    assign_public_ip = true
+  }
+
+  lifecycle {
+    ignore_changes = [task_definition]
+  }
+}
+
+resource "aws_ecs_task_definition" "placeholder_task" {
+  family                   = "placeholder-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256" # 0.25 vCPU
+  memory                   = "512" # 0.5 GB memory
+
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "placeholder-container"
+      image     = "amazonlinux:latest"
+      cpu       = 256
+      memory    = 512
+      essential = true
+      command   = ["sleep", "200"]
+    }
+  ])
 }
 
 resource "aws_iam_role" "ecs_task_role" {
